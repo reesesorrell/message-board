4f575f7877bcd9750967bd5cbbef16841d26cafa
Reese Sorrell <joreesesorrell@gmail.com>
2023-08-09 14:35:03 -0500

    Add messaging app capabilities

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages:messages });
 });
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
